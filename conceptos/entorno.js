7f96b80775ec0ca186f1716704848b1f46120c0a
mapecode <mario.perez17@alu.uclm.es>
2020-07-08 18:41:21 +0200

    comments added for exec with enviroment variables

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -1,5 +1,5 @@
-let nombre = process.env.NOMBRE || 'Sin nombre';
-let web = process.env.MI_WEB || 'no tengo web';
+let nombre = process.env.NOMBRE || 'Sin nombre' /* default */; // NOMBRE=Mario node conceptos/entorno.js
+let web = process.env.MI_WEB || 'no tengo web' /* default */;
 
 console.log('Hola '+ nombre);
 console.log('Mi web es '+ web);
